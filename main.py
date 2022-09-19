0bcecec31d38a7af7ecb070cb44c6c6dc9344f7f
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-09-19 15:21:53 +0530

    Search for badminton courts

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ import os
 
 driver = webdriver.Firefox() #use the firefox webdriver (geckodriver)
 driver.get('https://www.google.com/maps/@12.9601203,77.5955137,12.12z') #open google maps
-driver.implicitly_wait(3) #wait for 3 seconds if webpage loads slowly
-searchbox = driver.find_element_by_id('searchboxinput') #find the searchbox
-searchbox.click #click on the searchbox
+driver.implicitly_wait(5) #wait for 3 seconds if webpage loads slowly
+searchbox = driver.find_element(By.ID, 'searchboxinput') #find the searchbox
+driver.implicitly_wait(5)
+searchbox.click() #click on the searchbox
+driver.implicitly_wait(1)
+searchbox.send_keys('badminton courts')
+driver.implicitly_wait(5)
+searchbutton = driver.find_element(By.ID, 'searchbox-searchbutton')
+searchbutton.click()
+
 
