b100662429ebc314e7fdacc2a03acec9e0a572cd
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-09-19 15:04:42 +0530

    Open maps on firefox and click on the search box

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-
-
-
+'''
+    Badminton Search
+    Anirudh Koteshwar
+    19-09-2022
+'''
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os 
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox() #use the firefox webdriver (geckodriver)
+driver.get('https://www.google.com/maps/@12.9601203,77.5955137,12.12z') #open google maps
+driver.implicitly_wait(3) #wait for 3 seconds if webpage loads slowly
+searchbox = driver.find_element_by_id('searchboxinput') #find the searchbox
+searchbox.click #click on the searchbox
+
