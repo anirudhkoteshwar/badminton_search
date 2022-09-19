054ec980ff5fd618bf95b40c681bd6a331862ead
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-09-19 15:59:46 +0530

    write results to file

--- main.py
+++ main.py
@@ -7,18 +7,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import os 
+import time 
+import sys
 
-driver = webdriver.Firefox() #use the firefox webdriver (geckodriver)
-driver.get('https://www.google.com/maps/@12.9601203,77.5955137,12.12z') #open google maps
-driver.implicitly_wait(5) #wait for 3 seconds if webpage loads slowly
-searchbox = driver.find_element(By.ID, 'searchboxinput') #find the searchbox
+#use the firefox webdriver (geckodriver)
+driver = webdriver.Firefox() 
+
+#open google maps
+driver.get('https://maps.google.com') 
 driver.implicitly_wait(5)
-searchbox.click() #click on the searchbox
-driver.implicitly_wait(1)
-searchbox.send_keys('badminton courts')
+
+#find the searchbox and click on it
+searchbox = driver.find_element(By.ID, 'searchboxinput') 
 driver.implicitly_wait(5)
+searchbox.click()
+
+#search for badminton courts
+searchbox.send_keys('badminton courts in India')
+time.sleep(5)
 searchbutton = driver.find_element(By.ID, 'searchbox-searchbutton')
 searchbutton.click()
 
+#find all court names
+entries = driver.find_elements(By.CLASS_NAME, 'hfpxzc')
+
+for entry in entries:
+    name = entry.get_attribute("aria-label")
+    with open("courts.txt", 'a') as file:
+        file.write(f"{name}\n")
+
+
+#terminate the session
+driver.close() 
+driver.quit() 
 
